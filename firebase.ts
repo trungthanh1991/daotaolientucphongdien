a1403ee32218a8bab57614bdf65bda1b64c6358f
trungthanh1991 <trungthanh1991@gmail.com>
2025-10-31 20:18:16 +0700

    upload lan 1

--- firebase.ts
+++ firebase.ts
@@ -4,13 +4,13 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = { 
-  apiKey : "AIzaSyBv-fCE1zGvZcMq2r0fPm6WNCY9g7sz0nM" , 
-  authDomain : "dtltsl.firebaseapp.com" , 
-  projectId : "dtltsl" , 
-  storageBucket : "dtltsl.firebasestorage.app" , 
-  messagingSenderId : "254821251307" , 
-  appId : "1:254821251307:web:66be828f143709bc8926ae" , 
-  measurementId : "G-H8QWZGRWHZ" 
+  apiKey : "AIzaSyBRGDuQG-Nln-xwdezcGXlYIdyp-qWUZPk" , 
+  authDomain : "dtltpdien.firebaseapp.com" , 
+  projectId : "dtltpdien" , 
+  storageBucket : "dtltpdien.firebasestorage.app" , 
+  messagingSenderId : "571071073763" , 
+  appId : "1:571071073763:web:f9bd296c8bc8eed876e4d6" , 
+  measurementId : "G-Z8L5QNSL95" 
 };
 
 // Initialize Firebase
